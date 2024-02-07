0eabb365bcb22f51de5a05392ae359d22fedbe5b
terryweis <weist2088@gmail.com>
2024-02-06 23:24:29 -0600

    Step3Done

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,9 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +22,18 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+@PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType , @RequestParam String searchTerm){
+        if(searchTerm == "" || searchTerm.toLowerCase() == "all"){
+            model.addAttribute("jobs", JobData.findAll());
+            model.addAttribute("columns",columnChoices);
+        }
+        else{
+            model.addAttribute("jobs", JobData.findByColumnAndValue(searchType,searchTerm));
+            model.addAttribute("columns",columnChoices);
+        }
 
+        return "results";
+}
 }
 
